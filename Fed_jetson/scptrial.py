e9e398509f62d489dd632eb363b94c43ca94643c
Mihir Jog <132018573+mihirjog-iitb@users.noreply.github.com>
2024-06-30 20:19:31 +0530

    Update scptrial.py

--- Fed_jetson/scptrial.py
+++ Fed_jetson/scptrial.py
@@ -6,13 +6,15 @@ while(True):
 		char= fd.read(1)
 	if char=='r':       #send the file to server
 		print("r is found performing scp")
-		command = "scp net1.pt vip-lab@10.107.47.157:/home/vip-lab/Downloads/workspace/net1.pt"
+		#MJ: make sure to use proper username, and IP address in the command (as per server machine)
+		command = "scp net1.pt vip-lab@10.107.47.157:/home/vip-lab/Downloads/workspace/net1.pt" # MJ: make sure when you use this file in second client (device), you change the destination file name. eg. net2.pt 
 
 		output = subprocess.run(command, shell=True, check=True)
 
 		sleep(1)
 
-		command = "scp feat_dataset.pkl vip-lab@10.107.47.157:/home/vip-lab/Downloads/workspace/feat_dataset_1.pkl"
+		command = "scp feat_dataset.pkl vip-lab@10.107.47.157:/home/vip-lab/Downloads/workspace/feat_dataset_1.pkl" # MJ:make sure when you use this file in second client (device), you change the destination file name. eg. feat_dataset_2.pkl 
+
 		
 		output = subprocess.run(command, shell=True, check=True)
 
